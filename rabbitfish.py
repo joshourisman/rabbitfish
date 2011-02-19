02cf48fd8b78ce1400cb60e3c5d19411bfe8a7e7
Josh Ourisman <josh@joshourisman.com>
2011-02-19 11:29:50 -0500

    creating output directory if it doesn't already exist

--- rabbitfish.py
+++ rabbitfish.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from jinja2 import Environment, FileSystemLoader
+import os
 import yaml
 
 env = Environment(loader=FileSystemLoader('templates'))
@@ -8,4 +9,6 @@ print("One dead, unjugged rabbitfish later...")
 config = yaml.load(open("config.yaml", 'r'))
 
 index = env.get_template('index.html')
+if not os.path.exists("output"):
+    os.makedirs("output")
 open("output/index.html", 'w').write(index.render(**config))
