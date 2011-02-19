30efad818795711c54862e853d94d14917d5c35a
Josh Ourisman <josh@joshourisman.com>
2011-02-19 16:54:07 -0500

    moved Page and DynamicPage into pages.py

--- rabbitfish/__init__.py
+++ rabbitfish/__init__.py
@@ -1,61 +1,13 @@
 #!/usr/bin/env python
-from jinja2 import Environment, FileSystemLoader
-import datetime
 import os
 import shutil
 import yaml
 
-class Page(yaml.YAMLObject):
-    yaml_tag = '!Page'
-
-    def __setstate__(self, state):
-        if 'url' not in state:
-            state['url'] = "{}.html".format(state['name'])
-        state['output'] = "output/{}".format(state['url'])
-        state['directory'] = os.path.dirname(state['output'])
-        self.__dict__.update(state)
-
-    def render_to_string(self):
-        print("Rendering page {0} with {1}.".format(self.name, self.template))
-        template = env.get_template(self.template)
-        content = yaml.load(open("content/{}.yaml".format(self.name)))
-        return template.render(**content)
-
-    def render_to_output(self):
-        if not os.path.exists(self.directory):
-            os.makedirs(self.directory)
-        open(self.output, 'w').write(self.render_to_string())
-
-class DynamicPage(Page):
-    yaml_tag = '!DynamicPage'
-
-    def __setstate__(self, state):
-        if 'url' not in state:
-            state['url'] = "{}.html"
-        self.__dict__.update(state)
-
-    def render_to_string(self):
-        print("Rendering dynamic page {0} with {1}".format(
-                self.name, self.template))
-        template = env.get_template(self.template)
-        content_list = yaml.load_all(open("content/{}.yaml".format(self.name)))
-        return {content['slug']:template.render(**content) for
-                content in content_list}
-        
-    def render_to_output(self):
-        pages = self.render_to_string()
-        for slug in pages:
-            url = self.url.format(slug=slug, date=datetime.date.today())
-            output = "output/{}".format(url)
-            directory = os.path.dirname(output)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            open(output, 'w').write(pages[slug])
+from pages import Page, DynamicPage
 
 if __name__ == "__main__":
     print("One dead, unjugged rabbitfish later...")
 
-    env = Environment(loader=FileSystemLoader('templates'))
     config = yaml.load(open("config.yaml", 'r'))
     pages = config['pages']
     if os.path.exists('output'):
---
+++ rabbitfish/pages.py
@@ -0,0 +1,55 @@
+import datetime
+import os
+import yaml
+
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('templates'))
+
+class Page(yaml.YAMLObject):
+    yaml_tag = '!Page'
+
+    def __setstate__(self, state):
+        if 'url' not in state:
+            state['url'] = "{}.html".format(state['name'])
+        state['output'] = "output/{}".format(state['url'])
+        state['directory'] = os.path.dirname(state['output'])
+        self.__dict__.update(state)
+
+    def render_to_string(self):
+        print("Rendering page {0} with {1}.".format(self.name, self.template))
+        template = env.get_template(self.template)
+        content = yaml.load(open("content/{}.yaml".format(self.name)))
+        return template.render(**content)
+
+    def render_to_output(self):
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+        open(self.output, 'w').write(self.render_to_string())
+
+class DynamicPage(Page):
+    yaml_tag = '!DynamicPage'
+
+    def __setstate__(self, state):
+        if 'url' not in state:
+            state['url'] = "{}.html"
+        self.__dict__.update(state)
+
+    def render_to_string(self):
+        print("Rendering dynamic page {0} with {1}".format(
+                self.name, self.template))
+        template = env.get_template(self.template)
+        content_list = yaml.load_all(open("content/{}.yaml".format(self.name)))
+        return {content['slug']:template.render(**content) for
+                content in content_list}
+        
+    def render_to_output(self):
+        pages = self.render_to_string()
+        for slug in pages:
+            url = self.url.format(slug=slug, date=datetime.date.today())
+            output = "output/{}".format(url)
+            directory = os.path.dirname(output)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            open(output, 'w').write(pages[slug])
+
