491ab8deccc69bc506de0351518d85033d10ff4a
Josh Ourisman <josh@joshourisman.com>
2011-02-19 13:55:28 -0500

    moving rendering process into Page class

--- rabbitfish.py
+++ rabbitfish.py
@@ -11,21 +11,24 @@ env = Environment(loader=FileSystemLoader('templates'))
 class Page(yaml.YAMLObject):
     yaml_tag = '!Page'
 
+    def __setstate__(self, state):
+        if 'url' not in state:
+            state['url'] = "{}.html".format(state['name'])
+        state['output'] = "output/{}".format(state['url'])
+        state['directory'] = os.path.dirname(state['output'])
+        self.__dict__.update(state)
+
+    def render(self):
+        print("Rendering page {0} with {1}.".format(self.name, self.template))
+        template = env.get_template(self.template)
+        content = yaml.load(open("content/{}.yaml".format(self.name)))
+        return template.render(**content)
+
 config = yaml.load(open("config.yaml", 'r'))
 pages = config['pages']
 if os.path.exists('output'):
     shutil.rmtree('output')
 for page in pages:
-    name = page.name
-    template = page.template
-    url = getattr(page, 'url', '{}.html'.format(name))
-    output = "output/{}".format(url)
-    print("Rendering page {0} to {1}.".format(name, output))
-
-    directory = os.path.dirname(output)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    template = env.get_template(template)
-    content = yaml.load(open("content/{}.yaml".format(name)))
-    open(output, 'w').write(template.render(**content))
-
+    if not os.path.exists(page.directory):
+        os.makedirs(page.directory)
+    open(page.output, 'w').write(page.render())
