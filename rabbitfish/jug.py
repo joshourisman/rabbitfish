23a7f6880a369d38db1800d03e263583a8c60562
Josh Ourisman <josh@joshourisman.com>
2012-02-10 12:58:26 -0500

    created Site class

--- rabbitfish/jug.py
+++ rabbitfish/jug.py
@@ -5,6 +5,30 @@ import yaml
 from pages import Page, DynamicPage, ListPage
 
 
+class Site(object):
+    def __init__(self, config_file=None):
+        self.config = None
+        self.pages = []
+        self.dynamic_pages = {}
+
+        if config_file is not None:
+            self.load_configuration(config_file)
+
+    def load_configuration(self, config_file):
+        self.config = yaml.load(open(config_file, 'r'))
+        self.pages = self.config['pages']
+        for page in self.pages:
+            if type(page) is DynamicPage:
+                self.dynamic_pages[page.name] = page
+
+    def generate(self):
+        if os.path.exists('output'):
+            shutil.rmtree('output')
+
+        for page in self.pages:
+            page.render_to_output()
+
+
 def generatesite():
     config = yaml.load(open("config.yaml", 'r'))
     pages = config['pages']
